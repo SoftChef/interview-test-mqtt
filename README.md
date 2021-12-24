cdbf39db3ae78da7e42af6d7587a1dbab4635f05
MinCheTsai <minche@softchef.com>
2021-12-24 22:38:40 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# replace this
+# SoftChef Interview Test - MQTT Connect test
+
+### MQTT Broker Endpoint: ________________
+
+Test time: 20 minutes
+
+### Write a node.js program.
+
+1. Find a javascript or node.js MQTT library.
+3. Connect to MQTT Broker through MQTT library.
+4. Subscribe "softchef/data" topic and check receive message per second.
+5. Publish message to "softchef/control" topic.
+6. Explain how does steps complete this test.
+
+### Additional
+
+1. What's the MQTT protocol?
+2. Why does IoT preferred MQTT?
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { App, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Instance, InstanceClass, InstanceSize, InstanceType, MachineImage, Peer, Port, SecurityGroup, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
-export class MyStack extends Stack {
+export class SoftChefInterviewTestMqttStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
-
-    // define resources here...
+    const vpc = Vpc.fromLookup(this, 'Vpc', {
+      isDefault: true,
+    });
+    const securityGroup = new SecurityGroup(this, 'MqttBrokerSecurityGroup', {
+      vpc: vpc,
+      allowAllOutbound: true,
+    });
+    // securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), 'SSH');
+    securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(1883), 'MQTT Broker');
+    const installMqttBroker: UserData = UserData.forLinux();
+    installMqttBroker.addCommands('sudo apt-get update -y');
+    installMqttBroker.addCommands('sudo apt-get install mosquitto -y');
+    installMqttBroker.addCommands('sudo systemctl start mosquitto');
+    const mqttBrokerInstance = new Instance(this, 'MqttServer', {
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      vpc: vpc,
+      machineImage: MachineImage.genericLinux({
+        'ap-northeast-1': 'ami-0822295a729d2a28e',
+      }),
+      securityGroup: securityGroup,
+      userData: installMqttBroker,
+      keyName: 'InterviewTestMqtt',
+    });
+    mqttBrokerInstance.applyRemovalPolicy(RemovalPolicy.DESTROY);
   }
 }
 
@@ -17,7 +40,7 @@ const devEnv = {
 
 const app = new App();
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
+new SoftChefInterviewTestMqttStack(app, 'SoftChefInterviewTestMqtt', { env: devEnv });
 // new MyStack(app, 'my-stack-prod', { env: prodEnv });
 
 app.synth();
